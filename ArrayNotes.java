1fdf25902a0ce2086394340315ee50394a45ec57
Geoff Schmit <gcschmit@gmail.com>
2021-11-23 08:33:25 -0600

    add second part of array notes

--- ArrayNotes.java
+++ ArrayNotes.java
@@ -83,6 +83,36 @@ public class ArrayNotes
         int[] moreOdds = odds;
         odds[2] = 6;
         System.out.println(moreOdds[2]);        // prints 6
+        
+        /*
+         * Enhanced for Loop
+         * 
+         *  Iterates over the elements in an array.
+         *  Similar to the "for value in ..." structure in Python
+         */
+        for(int odd : odds)
+        {
+            System.out.println(odd);
+        }
+        
+        /*
+         * Limitations of Enhanced for Loops
+         * 
+         *  The local variable (e.g., odd) contains a copy of the value of the element
+         *      in the array.
+         *      
+         *  We cannot change the value of the elements in the array.
+         *  We cannot easily determine the index of an element.
+         */
+        for(int odd : odds )
+        {
+            odd += 1;
+        }
+        
+        for(int odd : odds)
+        {
+            System.out.println(odd);
+        }
     }
 }
 
